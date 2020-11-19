7b115f82f59aec568bb76121805fa887023fd0bc
VenzelCompany <74688785+VenzelCompany@users.noreply.github.com>
2020-11-19 05:05:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    var $link = $("a")
+    var $window = $(".ws .prog")
+    $link.click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href')
+        var target = $(id).offset().top - 90
+        $('html,body').animate({
+            scrollTop: target
+        }, 500)
+    })
+    $(window).scroll(function () {
+        var scroll = $(this).scrollTop()
+        $link.each(function () {
+            var attr = $(this).attr('href');
+            var selector =$(attr).offset().top - 100;
+            if(scroll >=selector) {
+                $link.removeClass('turnon');
+                $(this).addClass('turnon')
+            }
+        })
+    })
+    var $click = $('.click')
+    $('.prog').on('click',function () {
+        $(this).next().addClass('active')
+    });
+    $('.fa-times-circle').on('click',function () {
+        $click
+        $($click).removeClass('active')
+    });
+    let options = {
+        startAngle: -1.55,
+        size: 280,
+        value: 0.80,
+        fill: {color: "#528983"}
+      }
+      $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+      function(event, progress, stepValue){
+        $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+      });
+      $(".dc .bar").circleProgress({
+        value: 0.80
+      });
+      $(".copywritting .bar").circleProgress({
+        value: 0.30
+      });
+      $(".education .bar").circleProgress({
+        value: 0.30
+      });
+      $(".vm .bar").circleProgress({
+        value: 0.80
+      });
+      $(".uu .bar").circleProgress({
+        value: 0.25
+      });
+      $(".smm .bar").circleProgress({
+        value: 0.01
+      });
+});
